1a09ccc158c2865af87ae92457f04906bf77ac39
abareux <abareux@student.42mulhouse.fr>
2024-11-18 17:10:50 +0000

    added protection to memcopy

--- sources/memory/memcopy.c
+++ sources/memory/memcopy.c
@@ -16,6 +16,8 @@ void	*cookie_mem_copy(void* src_mem, void *dst_mem, const t_cookie_memory_size s
 {
 	t_cookie_memory_size	index = 0;
 
+	if (!dst_mem)
+		return cookie_mem_dup(src_mem, size);
 	while(index < size)
 	{
 		*((unsigned char *)dst_mem + index) = *((unsigned char *)src_mem + index);
